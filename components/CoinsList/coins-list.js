e449ffed7489b39f6c08e64b4bda3e0369724e6c
rzngnam1402 <legiangnam1402@gmail.com>
2022-08-18 15:07:01 +0700

    add layout and coin components

---
+++ components/CoinsList/coins-list.js
@@ -0,0 +1,28 @@
+import Coins from "../Coins/coins"
+
+const CoinsList = (props) => {
+
+    const filteredCoins = props.filteredCoins
+    console.log(filteredCoins);
+    return (
+        <>
+            {filteredCoins.map(coin => {
+                return (
+                    <Coins
+                        key={coin.id}
+                        name={coin.name}
+                        id={coin.id}
+                        price={coin.current_price}
+                        symbol={coin.symbol}
+                        marketcap={coin.market_cap}
+                        volume={coin.total_volume}
+                        image={coin.image}
+                        priceChange={coin.price_change_percentage_24h}
+                    />
+                )
+            })}
+        </>
+    )
+}
+
+export default CoinsList
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
+import CoinsList from "../components/CoinsList/coins-list"
+import Layout from "../components/Layout/layout"
 import SearchBar from "../components/SearchBar/search-bar"
 
-const index = () => {
+const HomePage = (props) => {
     return (
-        <SearchBar />
+        <Layout>
+            <SearchBar />
+            <CoinsList filteredCoins={props.filteredCoins} />
+        </Layout>
     )
 }
 
-export default index
+export const getServerSideProps = async () => {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false");
+    const filteredCoins = await res.json();
+
+    if (!filteredCoins) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
+
+    return {
+        props: {
+            filteredCoins,
+        }
+    }
+}
+
+export default HomePage
